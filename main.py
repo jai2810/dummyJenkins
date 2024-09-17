cd4f56bbf3a969dcdb7d2fa459b6d03c2294de05
Jai Prakash Sharma <32808195+jai2810@users.noreply.github.com>
2024-09-17 23:44:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,24 +3,7 @@ import subprocess
 
 if __name__ == '__main__':
     print("Hello world with env variable.")
-    with open("envfile.txt", 'w') as f:
-        f.write("123")
-    # shell_command = 'export MY_ENV_VAR=my_value_from_command && env | grep MY_ENV_VAR'
-
-    # # Run the shell command and capture the output
-    # result = subprocess.run(shell_command, capture_output=True, text=True, shell=True, executable='/bin/bash')
-
-    # # Check if the command was successful
-    # if result.returncode == 0:
-    #     # Parse the output to get the environment variable
-    #     output = result.stdout.strip()
-    #     key, value = output.split('=', 1)
-
-    #     # Set the environment variable in Python
-    #     os.environ[key] = value
-
-    #     # Verify that the environment variable is set
-    #     print(f"{key} = {os.environ[key]}")
-    # else:
-    #     print(f"Command failed: {result.stderr}")
+    with open("master_env.properties", 'a') as f:
+        f.write("var1=val1")
+    
 
