7c6167685dad1c9640f39adc73b712aeacdc17f1
Jai Prakash Sharma <32808195+jai2810@users.noreply.github.com>
2024-09-17 17:04:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,23 @@ import subprocess
 
 if __name__ == '__main__':
     print("Hello world with env variable.")
-    shell_command = 'export MY_ENV_VAR=my_value_from_command && env | grep MY_ENV_VAR'
+    os.environ["env2"] = "jp2"
+    # shell_command = 'export MY_ENV_VAR=my_value_from_command && env | grep MY_ENV_VAR'
 
-    # Run the shell command and capture the output
-    result = subprocess.run(shell_command, capture_output=True, text=True, shell=True, executable='/bin/bash')
+    # # Run the shell command and capture the output
+    # result = subprocess.run(shell_command, capture_output=True, text=True, shell=True, executable='/bin/bash')
 
-    # Check if the command was successful
-    if result.returncode == 0:
-        # Parse the output to get the environment variable
-        output = result.stdout.strip()
-        key, value = output.split('=', 1)
+    # # Check if the command was successful
+    # if result.returncode == 0:
+    #     # Parse the output to get the environment variable
+    #     output = result.stdout.strip()
+    #     key, value = output.split('=', 1)
 
-        # Set the environment variable in Python
-        os.environ[key] = value
+    #     # Set the environment variable in Python
+    #     os.environ[key] = value
 
-        # Verify that the environment variable is set
-        print(f"{key} = {os.environ[key]}")
-    else:
-        print(f"Command failed: {result.stderr}")
+    #     # Verify that the environment variable is set
+    #     print(f"{key} = {os.environ[key]}")
+    # else:
+    #     print(f"Command failed: {result.stderr}")
 
