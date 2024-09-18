f69c24f46fa93c4ee6edd6b1c6f5e5b3763d60bb
Jai Prakash Sharma <32808195+jai2810@users.noreply.github.com>
2024-09-19 02:21:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ import sys
 
 if __name__ == '__main__':
     print("Hello world with env variable.")
-    status = 1
-    with open("env.properties", 'w') as f:
-        f.write("POLARIS_BUILD_NUM=J")
-    if status!=1:
-        sys.exit(0)
-    else:
-        sys.exit(1)
+    os.environ["POLARIS_BUILD"]=123
+    print(os.environ.get("POLARIS_BUILD"))
     
 
