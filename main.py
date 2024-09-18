beb45c2b29087d6c2f805f01e2ebea2b2ab90e95
Jai Prakash Sharma <32808195+jai2810@users.noreply.github.com>
2024-09-19 01:53:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import subprocess
 if __name__ == '__main__':
     print("Hello world with env variable.")
     with open("env.properties", 'w') as f:
-        f.write("POLARIS_BUILD_NUM=JP")
+        f.write("POLARIS_BUILD_NUM=J")
+    return s
     
 
