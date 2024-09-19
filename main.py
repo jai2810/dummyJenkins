13d3841ab7b90576b4fb201bcf4988332a3ef3f3
Jai Prakash Sharma <32808195+jai2810@users.noreply.github.com>
2024-09-19 19:23:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ import subprocess
 import sys
 
 if __name__ == '__main__':
-    print("Hello world with env variable.")
-    print(os.environ.get("BUILD_NUMBER"))
+    subprocess.call("mkdir logs", shell=True)
+    build_num=123
+    file_with_build_number_as_name = os.environ.get("BUILD_NUMBER")
+    path = f"/logs/{file_with_build_number_as_name}"
+    with open(path, 'w') as f:
+        s = f"{build_num}"
+        f.write(s)
+    log.info("File written successfully")
+    with open(path, 'r') as f:
+        log.info("reading file")
+        log.info(f.read())
     sys.exit(0)
     
 
