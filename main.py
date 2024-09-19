47dc9ddaf2d8b323db6c1e8cfc5626f76b947126
Jai Prakash Sharma <32808195+jai2810@users.noreply.github.com>
2024-09-19 19:35:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ import subprocess
 import sys
 
 if __name__ == '__main__':
-    subprocess.call("mkdir logs", shell=True)
     build_num=123
     file_with_build_number_as_name = os.environ.get("BUILD_NUMBER")
-    path = f"/logs/{file_with_build_number_as_name}"
-    with open(path, 'w') as f:
+    file_with_build_number_as_name += ".txt"
+    with open(file_with_build_number_as_name, 'w') as f:
         s = f"{build_num}"
         f.write(s)
     log.info("File written successfully")
-    with open(path, 'r') as f:
+    with open(file_with_build_number_as_name, 'r') as f:
         log.info("reading file")
         log.info(f.read())
     sys.exit(0)
