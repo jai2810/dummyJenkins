fb074afa1019d9bb7f9fd990582270b6dd5532a1
Jai Prakash Sharma <32808195+jai2810@users.noreply.github.com>
2024-09-19 20:01:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 if __name__ == '__main__':
     build_num=123
     file_with_build_number_as_name = os.environ.get("BUILD_NUMBER")
-    file_with_build_number_as_name += ".txt"
+    file_with_build_number_as_name += ".json"
     print(file_with_build_number_as_name)
     with open(file_with_build_number_as_name, 'w') as f:
         s = str.format("\"build_number\":{}", build_num)
