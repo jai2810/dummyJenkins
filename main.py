44bf2d17fd69c956e4ec64b7934c6312c09f60c0
Jai Prakash Sharma <32808195+jai2810@users.noreply.github.com>
2024-09-19 15:03:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 
 if __name__ == '__main__':
     print("Hello world with env variable.")
-    os.environ["POLARIS_BUILD"]="321"
+    print(os.environ.get("BUILD_NUMBER"))
+    sys.exit(0)
     
 
