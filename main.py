8f843aaa8122dfeed07c315fbe2b16a336f978c7
Jai Prakash Sharma <32808195+jai2810@users.noreply.github.com>
2024-09-19 19:36:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ if __name__ == '__main__':
     build_num=123
     file_with_build_number_as_name = os.environ.get("BUILD_NUMBER")
     file_with_build_number_as_name += ".txt"
+    print(file_with_build_number_as_name)
     with open(file_with_build_number_as_name, 'w') as f:
         s = f"{build_num}"
         f.write(s)
-    log.info("File written successfully")
     with open(file_with_build_number_as_name, 'r') as f:
-        log.info("reading file")
-        log.info(f.read())
+        print("reading file")
+        print(f.read())
     sys.exit(0)
     
 
